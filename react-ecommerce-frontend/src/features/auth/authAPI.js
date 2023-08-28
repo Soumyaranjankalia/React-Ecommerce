7108b42efd629f64ff8a8dc6e20d36b012f22da9
Soumyaranjankalia <a2soumya.ranjan@gmail.com>
2023-08-28 13:18:55 +0530

    part 2: Added Routing (Login/Signup done)

---
+++ react-ecommerce-frontend/src/features/auth/authAPI.js
@@ -0,0 +1,9 @@
+// A mock function to mimic making an async request for data
+export function fetchCount(amount = 1) {
+  return new Promise(async (resolve) =>{
+    const respons = await fetch('http://localhost:8080')
+    const data = await respons.json();
+    resolve({data});
+  }
+  );
+}
